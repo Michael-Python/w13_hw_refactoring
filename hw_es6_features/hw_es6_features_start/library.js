070050128da79faffd072ed8562606b875199643
Michael <admin@fillthegaplearning.co.uk>
2021-01-24 13:33:28 +0000

    Altered to class syntax

--- hw_es6_features/hw_es6_features_start/library.js
+++ hw_es6_features/hw_es6_features_start/library.js
@@ -1,23 +1,57 @@
-const Library = function (books = []) {
-  this.books = books;
-};
+let books = [];
 
-Library.prototype.bookCount = function () {
-  return this.books.length;
-};
 
-Library.prototype.addBook = function (newBook) {
-  this.books.push(newBook);
-};
+// const Library = function (books = []) {
+//   this.books = books;
+// };
 
-Library.prototype.addBooks = function (newBooks) {
-  newBooks.forEach(book => this.books.push(book));
-};
+class Library {
+  constructor(book, length){
+    this.book = book;
+    this.length = length
+    this.books = books;
+  }
 
-Library.prototype.printInventory = function () {
-  this.books.forEach((book) => {
-    console.log(`${book.title} by ${book.author}`);
-  });
-};
+  bookCount(){
+    return this.books.length;
+
+  }
+
+
+  addBook(newBook){
+    this.books.push(newBook);
+  }
+
+  addBooks(newBooks){
+    newBooks.forEach(book => this.books.push(book));
+  }
+
+  printInventory(){
+    this.books.forEach((book) => {
+      console.log(`${book.title} by ${book.author}`);
+    });
+
+  }
+}
+
+// Library.prototype.bookCount = function () {
+//   return this.books.length;
+// };
+
+
+
+// Library.prototype.addBook = function (newBook) {
+//   this.books.push(newBook);
+// };
+
+// Library.prototype.addBooks = function (newBooks) {
+//   newBooks.forEach(book => this.books.push(book));
+// };
+
+// Library.prototype.printInventory = function () {
+//   this.books.forEach((book) => {
+//     console.log(`${book.title} by ${book.author}`);
+//   });
+// };
 
 module.exports = Library;
